885349fbb1d7ce4330371c2edfe4f919620d5392
celizond <elizondo.celular@gmail.com>
2026-06-09 13:46:58 -0300

    more

--- app/src/main/java/com/catedra/feruturnos/ui/reservation/ReservationDetailScreen.kt
+++ app/src/main/java/com/catedra/feruturnos/ui/reservation/ReservationDetailScreen.kt
@@ -2,15 +2,36 @@ package com.catedra.feruturnos.ui.reservation
 
 import android.content.Context
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.GroupAdd
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
+import com.catedra.feruturnos.R
+import com.catedra.feruturnos.ui.contacts.ContactUser
 import com.catedra.feruturnos.ui.home.Reservation
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 import com.utsman.osmandcompose.DefaultMapProperties
 import com.utsman.osmandcompose.Marker
@@ -18,38 +39,10 @@ import com.utsman.osmandcompose.OpenStreetMap
 import com.utsman.osmandcompose.ZoomButtonVisibility
 import com.utsman.osmandcompose.rememberCameraState
 import com.utsman.osmandcompose.rememberMarkerState
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.foundation.layout.Row
-import androidx.compose.ui.graphics.Color
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.rememberCoroutineScope
-import kotlinx.coroutines.launch
-import com.google.firebase.firestore.FieldValue
-import com.catedra.feruturnos.ui.contacts.ContactUser
-import coil.compose.AsyncImage
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import com.google.firebase.auth.FirebaseAuth
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.GroupAdd
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material.icons.filled.AttachMoney
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
-import androidx.compose.ui.res.stringResource
-import com.catedra.feruturnos.R
 
 @Composable
 fun ReservationDetailScreen(
@@ -57,13 +50,11 @@ fun ReservationDetailScreen(
     onReservationCancelled: (String) -> Unit,
     onNavigateToContacts: (String) -> Unit
 ) {
-
     var reservation by remember { mutableStateOf<Reservation?>(null) }
-    var participants by remember {
-        mutableStateOf<List<ContactUser>>(emptyList())
-    }
+    var participants by remember { mutableStateOf<List<ContactUser>>(emptyList()) }
     var isLoading by remember { mutableStateOf(true) }
     var isCancelling by remember { mutableStateOf(false) }
+    var mostrarDialogoCancelarReserva by remember { mutableStateOf(false) }
 
     val scope = rememberCoroutineScope()
     val context = androidx.compose.ui.platform.LocalContext.current
@@ -71,7 +62,10 @@ fun ReservationDetailScreen(
     LaunchedEffect(Unit) {
         Configuration.getInstance().apply {
             userAgentValue = context.packageName
-            load(context, context.getSharedPreferences("osm_prefs", Context.MODE_PRIVATE))
+            load(
+                context,
+                context.getSharedPreferences("osm_prefs", Context.MODE_PRIVATE)
+            )
         }
     }
 
@@ -90,9 +84,7 @@ fun ReservationDetailScreen(
             val reservationData = reservation
 
             if (reservationData != null) {
-
                 val users = reservationData.participantsId.mapNotNull { uid ->
-
                     val userDoc = Firebase.firestore
                         .collection("users")
                         .document(uid)
@@ -103,7 +95,8 @@ fun ReservationDetailScreen(
                         uid = uid,
                         name = userDoc.getString("name") ?: "",
                         contactId = userDoc.getString("contactId") ?: "",
-                        photo = userDoc.getString("photo") ?: ""
+                        photo = userDoc.getString("photo") ?: "",
+                        celphone = userDoc.getString("celphone") ?: ""
                     )
                 }
 
@@ -141,7 +134,6 @@ fun ReservationDetailScreen(
     val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
     val isCreator = currentUserId == r.creatorId
 
-
     val mapPoint = r.placeLocation?.let {
         GeoPoint(it.latitude, it.longitude)
     }
@@ -152,6 +144,7 @@ fun ReservationDetailScreen(
         r.placeFieldType,
         r.placeName
     )
+
     val reservationDeletedSuccessfully =
         stringResource(R.string.reserva_eliminada_correctamente)
 
@@ -163,6 +156,143 @@ fun ReservationDetailScreen(
 
     val unsubscribeText =
         stringResource(R.string.darse_de_baja)
+
+    val tituloDialogoCancelar =
+        if (isCreator) cancelReservationText else unsubscribeText
+
+    val mensajeDialogoCancelar =
+        if (isCreator) {
+            stringResource(R.string.seguro_que_queres_cancelar_reserva)
+        } else {
+            stringResource(R.string.seguro_que_queres_darte_de_baja)
+        }
+
+    if (mostrarDialogoCancelarReserva) {
+        Dialog(
+            onDismissRequest = {
+                if (!isCancelling) mostrarDialogoCancelarReserva = false
+            }
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 0.dp),
+                shape = RoundedCornerShape(0.dp),
+                color = Color.White
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = tituloDialogoCancelar,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+
+                        IconButton(
+                            onClick = {
+                                if (!isCancelling) {
+                                    mostrarDialogoCancelarReserva = false
+                                }
+                            },
+                            modifier = Modifier.align(Alignment.CenterEnd)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = stringResource(R.string.cancelar)
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = mensajeDialogoCancelar,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = 16.dp, bottom = 24.dp)
+                    )
+
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                try {
+                                    isCancelling = true
+
+                                    if (isCreator) {
+                                        val relatedUsers = r.participantsId.map { participantId ->
+                                            mapOf(
+                                                "userId" to participantId,
+                                                "read" to false
+                                            )
+                                        }
+
+                                        Firebase.firestore
+                                            .collection("notifications")
+                                            .add(
+                                                hashMapOf(
+                                                    "title" to titleCancelled,
+                                                    "message" to messageCancelled,
+                                                    "reservationId" to r.id,
+                                                    "relatedUsers" to relatedUsers,
+                                                    "createdAt" to FieldValue.serverTimestamp()
+                                                )
+                                            )
+                                            .await()
+
+                                        Firebase.firestore
+                                            .collection("reservations")
+                                            .document(reservationId)
+                                            .delete()
+                                            .await()
+
+                                        mostrarDialogoCancelarReserva = false
+                                        onReservationCancelled(reservationDeletedSuccessfully)
+                                    } else {
+                                        val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+                                        if (uid == null) {
+                                            isCancelling = false
+                                            return@launch
+                                        }
+
+                                        Firebase.firestore
+                                            .collection("reservations")
+                                            .document(reservationId)
+                                            .update(
+                                                mapOf(
+                                                    "participantsId" to FieldValue.arrayRemove(uid)
+                                                )
+                                            )
+                                            .await()
+
+                                        mostrarDialogoCancelarReserva = false
+                                        onReservationCancelled(leaveSuccessfully)
+                                    }
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                    isCancelling = false
+                                }
+                            }
+                        },
+                        enabled = !isCancelling,
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        if (isCancelling) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(20.dp),
+                                color = Color.White,
+                                strokeWidth = 2.dp
+                            )
+                        } else {
+                            Text(stringResource(R.string.confirmar))
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -185,7 +315,6 @@ fun ReservationDetailScreen(
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp)
         ) {
-
             Card(
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(
@@ -207,7 +336,6 @@ fun ReservationDetailScreen(
                             vertical = 16.dp
                         )
                 ) {
-
                     Text(
                         text = r.placeName,
                         color = Color.White,
@@ -232,7 +360,6 @@ fun ReservationDetailScreen(
                 elevation = CardDefaults.cardElevation(4.dp)
             ) {
                 Column(modifier = Modifier.padding(18.dp)) {
-
                     DetailRow(
                         icon = Icons.Default.LocationOn,
                         title = stringResource(R.string.cancha),
@@ -302,11 +429,20 @@ fun ReservationDetailScreen(
                                     text = participant.name,
                                     style = MaterialTheme.typography.titleMedium
                                 )
+
                                 Text(
                                     text = "#${participant.contactId}",
                                     color = Color.Gray,
                                     style = MaterialTheme.typography.bodySmall
                                 )
+
+                                if (participant.celphone.isNotBlank()) {
+                                    Text(
+                                        text = participant.celphone,
+                                        color = Color.Gray,
+                                        style = MaterialTheme.typography.bodySmall
+                                    )
+                                }
                             }
                         }
                     }
@@ -330,7 +466,9 @@ fun ReservationDetailScreen(
                     imageVector = Icons.Default.GroupAdd,
                     contentDescription = null
                 )
+
                 Spacer(modifier = Modifier.width(8.dp))
+
                 Text(stringResource(R.string.invitar_contactos))
             }
 
@@ -338,62 +476,7 @@ fun ReservationDetailScreen(
 
             Button(
                 onClick = {
-                    scope.launch {
-                        try {
-                            isCancelling = true
-
-                            if (isCreator) {
-                                val relatedUsers = r.participantsId.map { participantId ->
-                                    mapOf(
-                                        "userId" to participantId,
-                                        "read" to false
-                                    )
-                                }
-
-                                Firebase.firestore
-                                    .collection("notifications")
-                                    .add(
-                                        hashMapOf(
-                                            "title" to titleCancelled,
-                                            "message" to messageCancelled,
-                                            "reservationId" to r.id,
-                                            "relatedUsers" to relatedUsers,
-                                            "createdAt" to FieldValue.serverTimestamp()
-                                        )
-                                    )
-                                    .await()
-
-                                Firebase.firestore
-                                    .collection("reservations")
-                                    .document(reservationId)
-                                    .delete()
-                                    .await()
-                                onReservationCancelled(reservationDeletedSuccessfully)
-                            } else {
-                                val uid = FirebaseAuth.getInstance().currentUser?.uid
-
-                                if (uid == null) {
-                                    isCancelling = false
-                                    return@launch
-                                }
-
-                                Firebase.firestore
-                                    .collection("reservations")
-                                    .document(reservationId)
-                                    .update(
-                                        mapOf(
-                                            "participantsId" to FieldValue.arrayRemove(uid)
-                                        )
-                                    )
-                                    .await()
-
-                                onReservationCancelled(leaveSuccessfully)
-                            }
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            isCancelling = false
-                        }
-                    }
+                    mostrarDialogoCancelarReserva = true
                 },
                 enabled = !isCancelling,
                 modifier = Modifier
@@ -405,7 +488,8 @@ fun ReservationDetailScreen(
                     contentColor = Color.White
                 )
             ) {
-                val deleteTextBtn = if (isCreator) cancelReservationText else unsubscribeText
+                val deleteTextBtn =
+                    if (isCreator) cancelReservationText else unsubscribeText
 
                 if (isCancelling) {
                     CircularProgressIndicator(
@@ -438,8 +522,7 @@ fun FixedReservationMap(
     )
 
     Box(
-        modifier = modifier
-            .clipToBounds()
+        modifier = modifier.clipToBounds()
     ) {
         OpenStreetMap(
             modifier = Modifier.fillMaxSize(),
@@ -500,6 +583,7 @@ fun DetailRow(
                 color = Color.Gray,
                 style = MaterialTheme.typography.bodySmall
             )
+
             Text(
                 text = value,
                 style = MaterialTheme.typography.bodyLarge
